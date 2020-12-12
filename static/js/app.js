d1f65bb2359176a613783937d585d85fb2321dd4
Reid Haynie <reidhaynie@Reids-MacBook-Pro-2.local>
2020-12-12 13:30:08 -0600

    progress on app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,45 @@
 console.log("app.js is up")
 
-// data = d3.json("../data/samples.json")
-
-// let subject_name = 940
-
-// console.log(data)
+const jsonData = d3.json("../../data/samples.json")
+console.log(jsonData)
 
 
-// var data = [{
-//     type: 'bar',
-//     x: [data.],
-//     y: [],
-//     orientation: 'h'
-//   }];
-  
-//   Plotly.newPlot('myDiv', data);
+function init_build() {
+    d3.json("../../data/samples.json").then(
+        data => {
+        let selectSubject = d3.select("#selDataset")
+        let names=data.names;
+        names.forEach((d) => {
+            selectSubject.append("option")
+                .attr("value", d)
+                .text(d)
+        }) 
+})
+};
+init_build();
+
+function buildMetadata(d) {
+    d3.json("../../data/samples.json").then((obj) => {
+        let metadata = obj.metadata; 
+        let metadataArray = metadata.filter(obj => {obj.names == d});
+        result=metadataArray[0]
+        
+        let panelMetaData = d3.select("#sample-metadata");
+
+        panelMetaData.html("");
+
+        Object.entries(result).forEach(([key,value]) => 
+        panelMetaData.append("p").text(`${key}: ${value}`));
+    })  
+    };
+
+buildMetadata();
+      
+        // let resultArray = metadata.filter(obj => obj.id == sample);
+        // let result = resultArray[0];
+     
+        // let panel = d3.select("#sample-metadata");
+
+        // Object.entries(result).forEach([key, value]) => {
+        //     panel.append("h6").text(`${key}: ${value}`);
+        // }
