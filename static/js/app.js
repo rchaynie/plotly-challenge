b2878172186765b896c91cb23a63a168ec6e47d4
Reid Haynie <reidhaynie@Reids-MacBook-Pro-2.local>
2020-12-12 11:28:20 -0600

    added images

---
+++ static/js/app.js
@@ -0,0 +1,17 @@
+console.log("app.js is up")
+
+// data = d3.json("../data/samples.json")
+
+// let subject_name = 940
+
+// console.log(data)
+
+
+// var data = [{
+//     type: 'bar',
+//     x: [data.],
+//     y: [],
+//     orientation: 'h'
+//   }];
+  
+//   Plotly.newPlot('myDiv', data);
